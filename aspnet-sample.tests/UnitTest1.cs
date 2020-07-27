0d9769b0cfa172c1c7fd6c2dcefd653f3f6d9caa
Vishal Madhvani <vishal@vishalmadhvani.com>
2020-07-28 00:25:56 +0100

    add tests

---
+++ aspnet-sample.tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace aspnet_sample.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TrueShouldBeTrue()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod]
+        public void FalseShouldBeFalse()
+        {
+            Assert.AreEqual(false, false);
+        }
+    }
+}
